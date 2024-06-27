a325d4e8afd391c1bc89daa837df85e9c1349211
Moksleivis <Moksleivis@kitm.local>
2024-06-27 09:06:59 +0300

    first commit

---
+++ Valgykla/src/main/java/com/spring/Valgykla/ValgyklaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.Valgykla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValgyklaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValgyklaApplication.class, args);
+	}
+
+}
---
+++ Valgykla/src/test/java/com/spring/Valgykla/ValgyklaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.Valgykla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValgyklaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
