8f766a66c413f6c0b875b74ae4a998dbd144a0ed
0rose7 <sangokas36@gmail.com>
2024-06-27 09:15:58 +0300

    Spring Config

---
+++ Valgykla/src/main/java/com/spring/Valgykla/config/SpringConfig.java
@@ -0,0 +1,31 @@
+package com.spring.Valgykla.config;
+
+import com.spring.Valgykla.repository.UserRepository;
+//import com.spring.Valgykla.service.UserDetailsServiceImpl;
+import com.spring.Valgykla.service.UserService;
+import com.spring.Valgykla.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+public class SpringConfig {
+
+    @Autowired
+    public SpringConfig(UserRepository userRepository) {
+    }
+
+//    @Bean
+//    @Qualifier("UserDetailsService")
+//    public UserDetailsService getuserDetailsService() {
+//        return new UserDetailsServiceImpl();
+//    }
+
+    @Bean
+    @Qualifier("UserService")
+    public UserService getUserService(){
+        return new UserServiceImpl();
+    }
+}
